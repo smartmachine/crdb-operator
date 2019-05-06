5b8fb11e938360557527480d644348c4577da72a
Conrad Christensen <conrad.christensen@gmail.com>
2019-05-06 22:14:48 +0200

    Added the rest of the tests.

--- pkg/controller/cockroachdb/cockroach_test.go
+++ pkg/controller/cockroachdb/cockroach_test.go
@@ -3,8 +3,10 @@ package cockroachdb
 import (
 	"context"
 	dbv1alpha1 "go.smartmachine.io/crdb-operator/pkg/apis/db/v1alpha1"
+	appsv1 "k8s.io/api/apps/v1"
+	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
-	"k8s.io/api/policy/v1beta1"
+	policyv1beta1 "k8s.io/api/policy/v1beta1"
 	rbacv1 "k8s.io/api/rbac/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -27,11 +29,8 @@ var (
 	storagePerNode      = "100Gi"
 	maxUnavailableNodes = 1
 
-	cl                        client.Client
-	r                         ReconcileCockroachDB
-	req                       reconcile.Request
-	reqNoNamespace            reconcile.Request
-
+	cl client.Client
+	r  ReconcileCockroachDB
 )
 
 func TestMain(m *testing.M) {
@@ -52,199 +51,143 @@ func TestMain(m *testing.M) {
 		},
 	}
 
-	objs := []runtime.Object{crdb}
+	pod1 := &corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: name + "-1",
+			Namespace: namespace,
+			Labels: map[string]string{
+				"app": name,
+			},
+		},
+		Spec: corev1.PodSpec{
+			Containers: []corev1.Container{{
+				Name: "cockroachdb",
+				Image: "cockroachdb/cockroach:2.1.6",
+			}},
+		},
+		Status: corev1.PodStatus{
+			ContainerStatuses: []corev1.ContainerStatus{{
+				Name: "cockroachdb",
+				Ready: true,
+			}},
+		},
+	}
+
+	pod2 := &corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: name + "-2",
+			Namespace: namespace,
+			Labels: map[string]string{
+				"app": name,
+			},
+		},
+		Spec: corev1.PodSpec{
+			Containers: []corev1.Container{{
+				Name: "cockroachdb",
+				Image: "cockroachdb/cockroach:2.1.6",
+			}},
+		},
+		Status: corev1.PodStatus{
+			ContainerStatuses: []corev1.ContainerStatus{{
+				Name: "cockroachdb",
+				Ready: true,
+			}},
+		},
+	}
+
+	pod3 := &corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: name + "-3",
+			Namespace: namespace,
+			Labels: map[string]string{
+				"app": name,
+			},
+		},
+		Spec: corev1.PodSpec{
+			Containers: []corev1.Container{{
+				Name: "cockroachdb",
+				Image: "cockroachdb/cockroach:2.1.6",
+			}},
+		},
+		Status: corev1.PodStatus{
+			ContainerStatuses: []corev1.ContainerStatus{{
+				Name: "cockroachdb",
+				Ready: true,
+			}},
+		},
+	}
+
+
+
+	objs := []runtime.Object{crdb, pod1, pod2, pod3}
 
 	s := scheme.Scheme
 	s.AddKnownTypes(dbv1alpha1.SchemeGroupVersion, crdb)
+	s.AddKnownTypes(corev1.SchemeGroupVersion, pod1, pod2, pod3)
 
 	cl = fake.NewFakeClient(objs...)
 
-
 	r = ReconcileCockroachDB{client: cl, scheme: s}
 
-	req = reconcile.Request{
-		NamespacedName: types.NamespacedName{
-			Name:      name,
-			Namespace: namespace,
-		},
-	}
-
-	reqNoNamespace = reconcile.Request{
-		NamespacedName: types.NamespacedName{
-			Name: name,
-		},
-	}
 	os.Exit(m.Run())
 }
 
 func TestCockroachDBController(t *testing.T) {
-	t.Run("TestServiceAccount",      ServiceAccount)
-	t.Run("TestRole",                Role)
-	t.Run("TestRoleBinding",         RoleBinding)
-	t.Run("TestClusterRole",         ClusterRole)
-	t.Run("TestClusterRoleBinding",  ClusterRoleBinding)
-	t.Run("TestPublicService",       PublicService)
-	t.Run("TestService",             Service)
-	t.Run("TestPodDisruptionBudget", PodDisruptionBudget)
-	t.Run("TestStatefulSet",         StatefulSet)
-}
 
-func ServiceAccount(t *testing.T) {
-	res, err := r.Reconcile(req)
-	if err != nil {
-		t.Fatalf("reconcile: (%v)", err)
-	}
-	// Check the result of reconciliation to make sure it has the desired state.
-	if !res.Requeue {
-		t.Error("reconcile did not requeue request as expected")
-	}
-
-	// Check if deployment has been created and has the correct size.
-	sa := &corev1.ServiceAccount{}
-	err = cl.Get(context.TODO(), req.NamespacedName, sa)
-	if err != nil {
-		t.Fatalf("get serviceaccount: (%+v)", err)
-	}
-}
-
-// Test if Role is created
-func Role(t *testing.T) {
-	res, err := r.Reconcile(req)
-	if err != nil {
-		t.Fatalf("reconcile: (%v)", err)
-	}
-	// Check the result of reconciliation to make sure it has the desired state.
-	if !res.Requeue {
-		t.Error("reconcile did not requeue request as expected")
+	var tests = []struct {
+		description string
+		name        string
+		namePostfix string
+		namespace   string
+		object      runtime.Object
+	}{
+		{description: "TestServiceAccount",      name: name, namespace: namespace, object: &corev1.ServiceAccount{}},
+		{description: "TestRole",                name: name, namespace: namespace, object: &rbacv1.Role{}},
+		{description: "TestRoleBinding",         name: name, namespace: namespace, object: &rbacv1.RoleBinding{}},
+		{description: "TestClusterRole",         name: name, namespace: "",        object: &rbacv1.ClusterRole{}},
+		{description: "TestClusterRoleBinding",  name: name, namespace: "",        object: &rbacv1.ClusterRoleBinding{}},
+		{description: "TestPublicService",       name: name, namespace: namespace, object: &corev1.Service{}, namePostfix: "-public"},
+		{description: "TestService",             name: name, namespace: namespace, object: &corev1.Service{}},
+		{description: "TestPodDisruptionBudget", name: name, namespace: namespace, object: &policyv1beta1.PodDisruptionBudget{}},
+		{description: "TestStatefulSession",     name: name, namespace: namespace, object: &appsv1.StatefulSet{}},
+		{description: "TestBatchJob",            name: name, namespace: namespace, object: &batchv1.Job{}},
 	}
 
-	// Check if role has been created
-	role := &rbacv1.Role{}
-	err = cl.Get(context.TODO(), req.NamespacedName, role)
-	if err != nil {
-		t.Fatalf("get role: (%+v)", err)
-	}
-}
+	for _, test := range tests {
 
-// Test if RoleBinding is created
-func RoleBinding(t *testing.T) {
-	res, err := r.Reconcile(req)
-	if err != nil {
-		t.Fatalf("reconcile: (%v)", err)
-	}
-	// Check the result of reconciliation to make sure it has the desired state.
-	if !res.Requeue {
-		t.Error("reconcile did not requeue request as expected")
-	}
+		t.Run(test.description, func(t *testing.T) {
 
-	// Check if rolebinding has been created
-	role := &rbacv1.RoleBinding{}
-	err = cl.Get(context.TODO(), req.NamespacedName, role)
-	if err != nil {
-		t.Fatalf("get rolebinding: (%+v)", err)
-	}
-}
+			crNamespacedName := types.NamespacedName{
+				Name:      test.name,
+				Namespace: test.namespace,
+			}
 
-// Test if ClusterRole is created
-func ClusterRole(t *testing.T) {
-	res, err := r.Reconcile(reqNoNamespace)
-	if err != nil {
-		t.Fatalf("reconcile: (%v)", err)
-	}
-	// Check the result of reconciliation to make sure it has the desired state.
-	if !res.Requeue {
-		t.Error("reconcile did not requeue request as expected")
-	}
+			objNamespacedName := types.NamespacedName{
+				Name:      test.name + test.namePostfix,
+				Namespace: test.namespace,
+			}
 
-	// Check if clusterrole has been created
-	role := &rbacv1.ClusterRole{}
-	err = cl.Get(context.TODO(), reqNoNamespace.NamespacedName, role)
-	if err != nil {
-		t.Fatalf("get clusterrole: (%+v)", err)
-	}
-}
+			req := reconcile.Request{
+				NamespacedName: crNamespacedName,
+			}
 
-// Test if ClusterRoleBinding is created
-func ClusterRoleBinding(t *testing.T) {
-	res, err := r.Reconcile(reqNoNamespace)
-	if err != nil {
-		t.Fatalf("reconcile: (%v)", err)
-	}
-	// Check the result of reconciliation to make sure it has the desired state.
-	if !res.Requeue {
-		t.Error("reconcile did not requeue request as expected")
-	}
 
-	// Check if clusterrolebinding has been created
-	role := &rbacv1.ClusterRoleBinding{}
-	err = cl.Get(context.TODO(), reqNoNamespace.NamespacedName, role)
-	if err != nil {
-		t.Fatalf("get clusterrolebinding: (%+v)", err)
-	}
-}
 
-// Test if PublicService is created
-func PublicService(t *testing.T) {
-	customReq := reconcile.Request{
-		NamespacedName: types.NamespacedName{
-			Name:      name + "-public",
-			Namespace: namespace},
-	}
-	res, err := r.Reconcile(req)
-	if err != nil {
-		t.Fatalf("reconcile: (%v)", err)
-	}
-	// Check the result of reconciliation to make sure it has the desired state.
-	if !res.Requeue {
-		t.Error("reconcile did not requeue request as expected")
-	}
+			res, err := r.Reconcile(req)
+			if err != nil {
+				t.Fatalf("reconcile: (%+v)", err)
+			}
+			// Check the result of reconciliation to make sure it has the desired state.
+			if !res.Requeue {
+				t.Error("reconcile did not requeue request as expected")
+			}
 
-	// Check if publicservice has been created
-	role := &corev1.Service{}
-	err = cl.Get(context.TODO(), customReq.NamespacedName, role)
-	if err != nil {
-		t.Fatalf("get publicservice: (%+v)", err)
-	}
-}
+			// Check if object has been created
+			err = cl.Get(context.TODO(), objNamespacedName, test.object)
+			if err != nil {
+				t.Fatalf("get %T: (%+v)", test.object, err)
+			}
 
-// Test if Service is created
-func Service(t *testing.T) {
-	res, err := r.Reconcile(req)
-	if err != nil {
-		t.Fatalf("reconcile: (%v)", err)
-	}
-	// Check the result of reconciliation to make sure it has the desired state.
-	if !res.Requeue {
-		t.Error("reconcile did not requeue request as expected")
-	}
-
-	// Check if service has been created
-	role := &corev1.Service{}
-	err = cl.Get(context.TODO(), req.NamespacedName, role)
-	if err != nil {
-		t.Fatalf("get service: (%+v)", err)
-	}
-}
-
-// Test if PodDisruptionBudget is created
-func PodDisruptionBudget(t *testing.T) {
-	res, err := r.Reconcile(req)
-	if err != nil {
-		t.Fatalf("reconcile: (%v)", err)
-	}
-	// Check the result of reconciliation to make sure it has the desired state.
-	if !res.Requeue {
-		t.Error("reconcile did not requeue request as expected")
-	}
-
-	// Check if service has been created
-	obj := &v1beta1.PodDisruptionBudget{}
-	err = cl.Get(context.TODO(), req.NamespacedName, obj)
-	if err != nil {
-		t.Fatalf("get poddisruptionbudget: (%+v)", err)
-	}
-
-	if obj.Spec.MaxUnavailable.IntValue() != maxUnavailableNodes {
-		t.Fatalf("expected PodDisruptionBudget MaxUnavailable to be %d, got %d", maxUnavailableNodes, obj.Spec.MaxUnavailable.IntValue())
+		})
 	}
 }
